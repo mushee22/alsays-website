16137deff8a362fc63e38d04a9406ee126685e96
mushee22 <mmtmusheer@gmail.com>
2025-06-30 23:28:15 +0530

    feat: add new image assets for sustainability section and update logo files

---
+++ assets/index.ts
@@ -0,0 +1,66 @@
+import GridLayoutBg from "@/assets/images/background/grid-layers.png";
+import ContactCardBg from "@/assets/images/contact/contact-card-bg.jpg";
+import HomeAboutSectionImage from "@/assets/images/home/about-section.png";
+import HeroSectionImage from "@/assets/images/home/hero-section-image.png";
+import ServiceImage from "@/assets/images/service/service.png";
+import LogoDark from '@/assets/logo-dark.png';
+import Logo from '@/assets/logo.png';
+import FacebookIcon from '@/assets/svg/facebook.svg';
+import InstagramIcon from '@/assets/svg/instagram.svg';
+import ServiceIcon from "@/assets/svg/service.svg";
+import XIcon from '@/assets/svg/twitter.svg';
+import YoutubeIcon from '@/assets/svg/youtube.svg';
+
+import AboutHeroSectionBgImage from "@/assets/images/about/hero-bg.png";
+
+import ClientLogoOneImage from '@/assets/images/client/client-logo-1.png';
+import ClientLogoTwoImage from '@/assets/images/client/client-logo-2.png';
+import ClientLogoThreeImage from '@/assets/images/client/client-logo-3.png';
+
+import BlogImage from "@/assets/images/blog/blog.png";
+
+import CareerImage from "@/assets/images/career/career.png";
+
+import OurCultureImage from "@/assets/images/career/our-culture.png";
+import { default as BannerOneImage, default as BannerThreeImage } from "@/assets/images/home/banner-1.png";
+import BannerTwoImage from "@/assets/images/home/banner-2.png";
+
+import MarketPresenceMapImage from "@/assets/images/market-presence/market-presence-map.png";
+import MarketPresenceHeroImage from "@/assets/images/market-presence/market-presence.png";
+
+import SustainabilityAboutSectionImage from "@/assets/images/sustainability/about-sustainabilty.png";
+import SustainabilityHeroImage from "@/assets/images/sustainability/sustainability-hero-bg.png";
+
+import MenuSideImage from "@/assets/images/layout/menu-side-image.png";
+
+
+export {
+    AboutHeroSectionBgImage,
+    BannerOneImage,
+    BannerThreeImage,
+    BannerTwoImage,
+    BlogImage,
+    CareerImage,
+    ClientLogoOneImage,
+    ClientLogoThreeImage,
+    ClientLogoTwoImage,
+    ContactCardBg,
+    FacebookIcon,
+    GridLayoutBg,
+    HeroSectionImage,
+    HomeAboutSectionImage,
+    InstagramIcon,
+    Logo,
+    LogoDark,
+    MarketPresenceHeroImage,
+    MarketPresenceMapImage,
+    MenuSideImage,
+    OurCultureImage,
+    ServiceIcon,
+    ServiceImage,
+    SustainabilityAboutSectionImage,
+    SustainabilityHeroImage,
+    XIcon,
+    YoutubeIcon
+};
+
