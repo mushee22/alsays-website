f39dda82bc1ac6fd32a795d5d07b73ea3b2c8fea
mushee22 <mmtmusheer@gmail.com>
2025-07-27 00:28:55 +0530

    feat: update ProductCard component to use Strapi image URLs for product images

--- components/elements/product-card.tsx
+++ components/elements/product-card.tsx
@@ -1,25 +1,27 @@
+import { getStrapiImageURL } from "@/lib/config";
 import { Product } from "@/types";
 import Image from "next/image";
 
 export default function ProductCard(product: Product) {
-    return (
-        <div className="flex flex-col hover:shadow transition-all duration-500 cursor-pointer hover:rounded-2xl">
-            <div className="aspect-square bg-grey-100 rounded-2xl mb-3 md:mb-6 relative">
-                {
-                    product.image && product.image.length && product.image[0]?.url &&
-                    <Image
-                        src={product.image[0]?.url}
-                        alt={product.title ?? "Product Image"}
-                        fill
-                    />
-                }
-            </div>
-            {
-                product.title && <h3 className="text-primary title-xl md:mt-4">{product.title}</h3>
-            }
-            {
-                product.description && <p className="line-clamp-2 text-grey-800 body-sm md:mt-2.5">{product.description}</p>
-            }
-        </div>
-    )
+  return (
+    <div className="flex flex-col hover:shadow transition-all duration-500 cursor-pointer hover:rounded-2xl">
+      <div className="aspect-square bg-grey-100 rounded-2xl mb-3 md:mb-6 relative">
+        {product.image && product.image.length && product.image[0]?.url && (
+          <Image
+            src={getStrapiImageURL(product.image[0]?.url) ?? ""}
+            alt={product.title ?? "Product Image"}
+            fill
+          />
+        )}
+      </div>
+      {product.title && (
+        <h3 className="text-primary title-xl md:mt-4">{product.title}</h3>
+      )}
+      {product.description && (
+        <p className="line-clamp-2 text-grey-800 body-sm md:mt-2.5">
+          {product.description}
+        </p>
+      )}
+    </div>
+  );
 }
