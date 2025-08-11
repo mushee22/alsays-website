f9d7cac73ec4116ef0494a472f496a52e8f89a1f
mushee22 <mmtmusheer@gmail.com>
2025-08-11 22:23:38 +0530

    style(product-card): adjust spacing and margins for better visual consistency

--- components/elements/product-card.tsx
+++ components/elements/product-card.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function ProductCard(product: Product) {
   return (
     <div className="flex flex-col hover:shadow transition-all duration-500 cursor-pointer hover:rounded-2xl">
-      <div className="aspect-square bg-grey-100 rounded-2xl mb-3 md:mb-6 relative">
+      <div className="aspect-square bg-grey-100 rounded-2xl  relative">
         {product.image && product.image.length && product.image[0]?.url && (
           <Image
             src={getStrapiImageURL(product.image[0]?.url) ?? ""}
@@ -15,7 +15,7 @@ export default function ProductCard(product: Product) {
         )}
       </div>
       {product.title && (
-        <h3 className="text-primary title-xl md:mt-4 pl-2 pr-1">{product.title}</h3>
+        <h3 className="text-primary title-xl mt-3 pl-2 pr-1">{product.title}</h3>
       )}
       {product.description && (
         <p className="line-clamp-2 text-grey-800 body-sm md:mt-2.5 pl-2 pr-1">
